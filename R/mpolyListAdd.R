cf69f37a8e42ee60898be2b2ef40ba794df0f2df
david kahle <david.kahle@gmail.com>
2014-12-06 13:24:27 -0600

    Update mpolyListAdd.R

--- R/mpolyListAdd.R
+++ R/mpolyListAdd.R
@@ -17,22 +17,11 @@
 '+.mpolyList' <- function(e1, e2){
   
   ## argument check
+  if(is.numeric(e1) && length(e1) == 1) e1 <- mpoly(list(c(coef = e1)))
+  if(is.mpoly(e1)) e1 <- mpolyList(e1)
   
-  if(is.numeric(e1) && length(e1) == 1){
-    e1 <- mpoly(list(c(coef = e1)))
-  }
-  
-  if(is.mpoly(e1)){
-  	e1 <- mpolyList(e1)
-  }
-  
-  if(is.numeric(e2) && length(e2) == 1){
-    e2 <- mpoly(list(c(coef = e2)))
-  }  
-  
-  if(is.mpoly(e2)){
-  	e2 <- mpolyList(e2)
-  }  
+  if(is.numeric(e2) && length(e2) == 1) e2 <- mpoly(list(c(coef = e2)))
+  if(is.mpoly(e2)) e2 <- mpolyList(e2)
   
   if(!is.mpolyList(e1) || !is.mpolyList(e2)){
     stop('e1 and e2 must be of class mpolyList.', call. = FALSE)
@@ -41,7 +30,6 @@
   if(length(e1) != length(e2)){
     stop('e1 and e2 must have equal length.', call. = FALSE)
   }
-  
   
   
   ## determine length, flatten, and make indices on which to add
@@ -55,14 +43,11 @@
   ndcs2add <- split(cbind(1:n, (n+1):(2*n)), 1:n)
   
   
-  
-  
   ## sum
   out <- lapply(ndcs2add, function(v){
     Reduce('+', flatList[v])
   })
   out <- unname(out)
-  
   
   
   ## caste and return
