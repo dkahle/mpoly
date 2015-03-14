ffce1b31050331ba7502f653961eeff484b68cee
David Kahle <david.kahle@gmail.com>
2015-03-14 15:35:52 -0500

    single to double quotes, backticks for name

--- R/mpolyEqual.R
+++ R/mpolyEqual.R
@@ -10,23 +10,28 @@
 #' @name mpolyEqual
 #' @export
 #' @examples
-#' p1 <- mp('x + y + 2 z')
+#' p1 <- mp("x + y + 2 z")
 #' p1 == p1
 #' 
-#' p2 <- reorder(p1, order = 'lex', varorder = c('z','y','x'))
+#' p2 <- reorder(p1, order = "lex", varorder = c("z","y","x"))
 #' p1 == p2
-#' p2 <- reorder(p1, order = 'lex', varorder = c('z','w','y','x'))
+#' p2 <- reorder(p1, order = "lex", varorder = c("z","w","y","x"))
 #' p1 == p2
 #' p1 == ( 2 * p2 )
 #'
-#' mp('x + 1') == mp('y + 1')
-#' mp('2') == mp('1')
-#' mp('1') == mp('1')
-#' mp('0') == mp('-0')
-'==.mpoly' <- function(e1, e2){
+#' p1 <- mp("x + 1") 
+#' p2 <- mp("x + 1")
+#' identical(p1, p2)
+#' p1 == p2
+#'
+#' mp("x + 1") == mp("y + 1")
+#' mp("2") == mp("1")
+#' mp("1") == mp("1")
+#' mp("0") == mp("-0")
+`==.mpoly` <- function(e1, e2){
 	
   if(!is.mpoly(e1)  || !is.mpoly(e2)){
-    stop('e1 and e2 must be of class mpoly.', call. = FALSE)
+    stop("e1 and e2 must be of class mpoly.", call. = FALSE)
   }
   
   diff <- e1 - e2
