2025ef1359bc174f93ed29a3d398cfd72a7e48ea
David Kahle <david.kahle@gmail.com>
2015-04-30 17:27:43 -0500

    mpolyArithmetic condensed, mpolyListArithmetic condensed

---
+++ R/mpolyArithmetic.R
@@ -0,0 +1,121 @@
+#' Arithmetic with multivariate polynomials
+#'
+#' Arithmetic with multivariate polynomials
+#' 
+#' @param e1 an object of class mpoly
+#' @param e2 an object of class mpoly
+#' @return object of class mpoly
+#' @name mpolyArithmetic
+#' @examples
+#' 
+#' p <- mp("x + y")
+#' p + p
+#' p - p
+#' p * p
+#' p^2
+#' 
+#' 
+#' p + 1
+#' 2*p
+#' 
+#' 
+#' 
+NULL
+
+
+
+
+
+
+
+
+#' @rdname mpolyArithmetic
+#' @export
+`+.mpoly` <- function(e1, e2){
+	
+  mpoly( c( unclass(e1), unclass(e2) ) )
+ 
+}
+
+
+
+
+
+
+
+#' @rdname mpolyArithmetic
+#' @export
+`-.mpoly` <- function(e1, e2){
+  
+  ## flip coefficients of each term
+  e2 <- lapply(e2, function(v){
+    v["coef"] <- -v["coef"]
+    v
+  })
+  class(e2) <- "mpoly"
+  
+  ## add
+  e1 + e2	
+}
+
+
+
+
+
+
+#' @rdname mpolyArithmetic
+#' @export
+`*.mpoly` <- function(e1, e2){
+  
+  ## allow for multiplication by a constant
+  if(is.numeric(e1) && length(e1) == 1) e1 <- mpoly(list(c(coef = e1)))
+  
+  if(is.numeric(e2) && length(e2) == 1) e2 <- mpoly(list(c(coef = e2)))
+  
+  
+  
+  ## argument check
+  if(!is.mpoly(e1) || !is.mpoly(e2)){
+    stop('e1 and e2 must be of class mpoly.', call. = FALSE)
+  }
+  
+  
+  
+  ## multiply
+  list <- lapply(e1, function(v){
+    lapply(e2, function(z){
+      c(v, z)  
+    })	
+  })
+  
+  
+  
+  ## return
+  mpoly( unlist(list, recursive = FALSE) )
+}	
+
+
+
+
+
+
+
+
+
+
+#' @rdname mpolyArithmetic
+#' @export
+'^.mpoly' <- function(e1, e2){
+  
+  if(!is.mpoly(e1)) stop('e1 must be of class mpoly.', call. = FALSE)
+  
+  if(!is.wholenumber(e2) || e2 < 0) stop('exponent must be a positive integer.', call. = FALSE)
+  
+  if(e2 == 0) return(mpoly(list(c(coef = 1))))
+  
+  out <- mpoly(list(c(coef = 1)))
+  for(k in 1:e2){
+    out <- out * e1
+  }
+  out
+}
--- R/mpolyListArithmetic.R
+++ R/mpolyListArithmetic.R
@@ -12,6 +12,7 @@
 #' ( ms2 <- mp( c('x + 1', 'y + 2') ) )
 #' ms1 + ms2
 #' ms1 - ms2
+#' ms1 * ms2
 #' 
 NULL
 
@@ -93,6 +94,56 @@ NULL
 
 
 
+
+
+
+
+
+
+
+
+#' @rdname mpolyListArithmetic
+#' @export
+`*.mpolyList` <- function(e1, e2){
+  
+  ## argument check
+  
+  if(is.numeric(e1) && length(e1) == 1) e1 <- mpoly(list(c(coef = e1)))  
+  if(is.mpoly(e1)) e1 <- mpolyList(e1)
+  
+  if(is.numeric(e2) && length(e2) == 1) e2 <- mpoly(list(c(coef = e2)))  
+  if(is.mpoly(e2)) e2 <- mpolyList(e2)
+  
+  stopifnot(is.mpolyList(e1))
+  stopifnot(is.mpolyList(e2))
+  
+  if(length(e1) != length(e2)) stop('e1 and e2 must have equal length.', call. = FALSE)
+  
+  
+  
+  ## determine length, flatten, and make indices on which to multiply
+  n <- length(e1)
+  
+  flatList <- unlist(list(
+    unclass(e1),
+    unclass(e2)
+  ), recursive = FALSE)
+  
+  ndcs2add <- split(cbind(1:n, (n+1):(2*n)), 1:n)
+  
+  
+  
+  
+  ## multiply
+  out <- lapply(ndcs2add, function(v) Reduce('*', flatList[v]))
+  out <- unname(out)
+  
+  
+  
+  ## caste and return
+  class(out) <- 'mpolyList'
+  out
+}	
 
 
 
