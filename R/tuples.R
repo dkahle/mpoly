319fecdebdde7a504be70be95af822ce2ebf74f0
David Kahle <david.kahle@gmail.com>
2015-05-03 19:47:03 -0500

    tuples: reworked interior code, likely no real speed ups but a much better implementation

--- R/tuples.R
+++ R/tuples.R
@@ -1,6 +1,7 @@
 #' Determine all n-tuples using the elements of a set.
 #' 
-#' Determine all n-tuples using the elements of a set.
+#' Determine all n-tuples using the elements of a set.  This is really just a
+#' simple wrapper for expand.grid, so it is not optimized.
 #' 
 #' @param set a set
 #' @param n length of each tuple
@@ -8,42 +9,47 @@
 #' @return a matrix whose rows are the n-tuples
 #' @export
 #' @examples
+#' 
 #' tuples(1:2, 5)
-#' apply(tuples(c('x','y','z'), 3), 1, paste, collapse = '')
+#' apply(tuples(c("x","y","z"), 3), 1, paste, collapse = "")
 #' 
 #' # multinomial coefficients
 #' r <- 2 # number of variables, e.g. x, y
 #' n <- 2 # power, e.g. (x+y)^2
 #' apply(burst(n,r), 1, function(v) factorial(n)/ prod(factorial(v))) # x, y, xy
-#' mp('x + y')^n
+#' mp("x + y")^n
 #' 
 #' r <- 2 # number of variables, e.g. x, y
 #' n <- 3 # power, e.g. (x+y)^3
 #' apply(burst(n,r), 1, function(v) factorial(n)/ prod(factorial(v))) # x, y, xy
-#' mp('x + y')^n
+#' mp("x + y")^n
 #' 
 #' r <- 3 # number of variables, e.g. x, y, z
 #' n <- 2 # power, e.g. (x+y+z)^2
 #' apply(burst(n,r), 1, function(v) factorial(n)/ prod(factorial(v))) # x, y, z, xy, xz, yz
-#' mp('x + y + z')^n
-tuples <- function(set, n = length(set), repeats = FALSE){
+#' mp("x + y + z")^n
+#' 
+#' 
+tuples <- function(set, n = length(set), repeats = FALSE, list = FALSE){
+
+  ## determine how big the output will be
   r <- length(set)	
-  set2 <- 1:r
-  sets4call <- paste(rep('set2', length = n), collapse = ', ')
-  str4eval <- paste('expand.grid(', sets4call, 
-    ', stringsAsFactors=F, KEEP.OUT.ATTRS=F)', sep = '')
-  mat <- as.matrix(unname(eval(parse(text = str4eval))))
-  d <- dim(mat)
-  mat <- paste('_', mat, sep = '')
-  dim(mat) <- d
-  for(k in 1:r){
-    mat[mat == paste('_', k, sep='')] <- set[k]
-  }
-  if(is.numeric(set)){
-    mat <- as.numeric(mat)
-    dim(mat) <- d
-  }
-  if(!repeats) mat <- unique(mat)
-  mat
+  nCombos <- r^n
+  
+  ## expand.grid really does all the work in this function, so
+  ## setup the interior part of expand.grid.  
+  ## sets4call looks like "set2, set2, set2" with n = 3
+  out <- do.call(expand.grid, replicate(n, set, simplify = FALSE))
+  out <- unname(as.matrix(out))
+  out <- out[,n:1]
+  
+  ## delete duplicates
+  if(!repeats) out <- unique(out)
+  
+  ## do list
+  if(list) out <- unname(split(out, rep(1:nCombos, r)))
+  
+  ## return
+  out
 }
 
