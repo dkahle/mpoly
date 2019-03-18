82cbdb5af558a1ea951c96446156af5971f8f0c4
David Kahle <david.kahle@gmail.com>
2019-03-17 21:58:18 -0500

    finalize print.mpoly

--- R/print.mpoly.R
+++ R/print.mpoly.R
@@ -10,15 +10,17 @@
 #' @param silent logical; if TRUE, suppresses output
 #' @param ... additional parameters to go to [base::cat()]
 #' @param plus_pad number of spaces to the left and right of plus sign
-#' @param times_pad number of spaces to the left and right of multiplication
-#'   sign
+#' @param times_pad number of spaces to the left and right of times sign
 #' @usage \method{print}{mpoly}(x, varorder, order, stars = FALSE, silent =
 #'   FALSE, ..., plus_pad = 2L, times_pad = 1L)
 #' @return Invisible string of the printed object.
 #' @export
 #' @examples
-#' p <- mp("2 x^5  -  3 y^2  +  x y^3")
-#' p
+#' 
+#' mp("-x^5 - 3 y^2 + x y^3 - 1")
+#' 
+#' 
+#' (p <- mp("2 x^5  -  3 y^2  +  x y^3"))
 #' print(p) # same
 #' print(p, silent = TRUE)
 #' s <- print(p, silent = TRUE)
@@ -26,11 +28,12 @@
 #'
 #' print(p, order = "lex") # -> 2 x^5  +  x y^3  -  3 y^2
 #' print(p, order = "lex", varorder = c("y","x")) # -> y^3 x  -  3 y^2  +  2 x^5
-#' print(p, varorder = c("y","x")) # -> 2 x^5  -  3 y^2  +  y^3 x
+#' print(p, varorder = c("y","x")) # -> y^3 x  -  3 y^2  +  2 x^5
 #'
+#' # this is mostly used internally
 #' print(p, stars = TRUE)
-#'
-#' #' # this is mostly used internally
+#' print(p, stars = TRUE, times_pad = 0L)
+#' print(p, stars = TRUE, times_pad = 0L, plus_pad = 1L)
 #' print(p, plus_pad = 1L)
 #' print(p, plus_pad = 0L, stars = TRUE, times_pad = 0L)
 #' 
@@ -39,21 +42,18 @@ print.mpoly <- function(x, varorder, order, stars = FALSE, silent = FALSE, ..., 
   ## argument checking and basic variable setting
   stopifnot(is.mpoly(x))  
   stopifnot(is.logical(stars)) 
-  vars <- vars(x)
-  
+  vars <- vars(x) 
   
   
   ## deal with constant mpolys
-  if(length(vars) == 0){
-    if(!silent) cat(x[[1]], ...)
+  if (length(vars) == 0) {
+    if (!silent) cat(x[[1]], ...)
   	return(invisible(as.character(x[[1]])))
-  }
-  
-    
+  }  
   
   
   ## check variable order
-  if(!missing(varorder)){
+  if (!missing(varorder)) {
     stopifnot(is.character(varorder))
     if(!all(vars %in% varorder)) {
       stop(
@@ -64,81 +64,89 @@ print.mpoly <- function(x, varorder, order, stars = FALSE, silent = FALSE, ..., 
     }
   } 
   
-  if(!missing(order)) match.arg(order, c("lex", "glex", "grlex"))  
-  
-  
+  if (!missing(order)) match.arg(order, c("lex", "glex", "grlex"))    
   
   
   ## reorder
-  if(!missing(order) && !missing(varorder)){
+  if (!missing(order) && !missing(varorder)) {
     x <- reorder(x, varorder = varorder, order = order)
   } else if(!missing(varorder)){
     x <- reorder(x, varorder = varorder)
   } else if(!missing(order)){
     x <- reorder(x, order = order)	
-  }
-  
-  
+  }  
  
   
-  ## print with stars or not, ^ coded first then *
+  ## set delimiters
+  if (!stars) { 
+    
+    times <- " "
+    expo  <- "^"
   
-  if(!stars){    # regular printing ^
+  } else {
   	
-    ## make printed terms
-    terms <- sapply(x, function(v){
-      if(length(v) == 1) return(format(v[["coef"]], scientific=FALSE)) # if a constant term
-      v <- v[v != 0] # eliminate lingering x^0"s
-      p <- length(v) - 1
-      s <- paste(names(v[1:p]), v[1:p], sep = "^", collapse = " ")
-      s <- paste(format(v[["coef"]], scientific=FALSE), s)
-      if(substr(s, 1, 2) == "1 ") s <- substr(s, 3, nchar(s))
-      if(substr(s, 1, 3) == "-1 ") s <- paste("-", substr(s, 4, nchar(s)), sep = "")
-      s
-    })
-  
-    ## merge and pretty
-    plus_pad <- paste(rep(" ", plus_pad), collapse = "")
-    s <- paste(terms, collapse = paste0(plus_pad, "+", plus_pad))
-    s <- paste(" ", s, " ", sep = "") # insert " " pads
-    s <- gsub("\\^1 ", " ", s)        # remove ^1"s  
-    s <- gsub("\\+  -", "-  ", s)     # fix subtractions                       
-    s <- substr(s, 2, nchar(s) - 1)   # remove " " pads
-    if( substr(s, 1, 1) == "-" && x[[1]]["coef"] == -1 && length(x[[1]]) > 1 ){
-	    s <- paste("-1", substr(s, 2, nchar(s)))
-	  }
-    if(!silent) cat(s, ...)
-    return(invisible(s))
-  
-  } else {    # stars printing
-  	
-    ## make printed terms
-    terms <- sapply(x, function(v){
-      if(length(v) == 1) return(format(v[["coef"]], scientific=FALSE)) # if a constant term
-      p <- length(v) - 1
-      times_pad <- paste(rep(" ", times_pad), collapse = "")
-      s <- paste(names(v[1:p]), v[1:p], sep = "**", collapse = paste0(times_pad, "*", times_pad))
-      s <- paste(format(v[["coef"]], scientific=FALSE), s, sep = paste0(times_pad, "*", times_pad))
-      if(substr(s, 1, 4) == "1 * ") s <- substr(s, 5, nchar(s))
-      if(substr(s, 1, 5) == "-1 * ") s <- paste("-", substr(s, 6, nchar(s)), sep = "")
-      s      
-    })
-  
-    ## merge and pretty
-    plus_pad <- paste(rep(" ", plus_pad), collapse = "")
-    s <- paste(terms, collapse = paste0(plus_pad, "+", plus_pad))
-    s <- paste(" ", s, " ", sep = "") # insert " " pads
-    s <- gsub("\\*\\*1 ", " ", s)     # remove ^1"s  
-    s <- gsub("\\+  -", "-  ", s)     # fix subtractions      
-    s <- substr(s, 2, nchar(s) - 1)   # remove " " pads
-    if( substr(s, 1, 1) == "-" && x[[1]]["coef"] == -1 ){
-	    s <- paste("-1 *", substr(s, 2, nchar(s)))
-	  }    
-    if(!silent) cat(s, ...)
-    return(invisible(s))  	
+    times_pad <- stringi::stri_dup(" ", times_pad)
+    times <- stringi::stri_c(times_pad, "*", times_pad)
+    expo  <- "**"
       	
-  }
+  } 
+  
+  
+  ## print terms
+  terms <- vapply(x, print_term, character(1L), times = times, exp = expo) 
+  
+  
+  ## merge and pretty
+  plus_pad <- stringi::stri_dup(" ", plus_pad)
+  plus <- stringi::stri_c(plus_pad, "+", plus_pad)
+  s <- stringi::stri_c(terms, collapse = plus)
+  s <- stringi::stri_replace_all_fixed(s, stringi::stri_c(expo, "1", times), times)  # remove ^1"s  
+  s <- stringi::stri_replace_all_fixed(s,              # fix subtractions 
+    stringi::stri_c("+", plus_pad, "-"), 
+    stringi::stri_c("-", plus_pad)
+  )  
+  
+  
+  ## print
+  if(!silent) cat(s, ...)
+  
+  
+  ## return
+  invisible(s)
+  
 }
 
 
+
+
+
+
+
+
+
+print_term <- function(term, times = " ", exp = "^") {
+  
+  # if a constant term
+  if (length(term) == 1L) return(format(term[["coef"]], scientific=FALSE))
+  
+  # split vars + exps from coef
+  coef_ndx <- which(names(term) == "coef")
+  coef <- term[[ coef_ndx]]
+  mono <- term [-coef_ndx]
+  formatted_coef <- format(coef, scientific = FALSE)
+  
+  # squeeze together to, e.g. x^1 y^2
+  s <- stringi::stri_c(names(mono), mono, sep = exp, collapse = times)
+  
+  # paste on formatted coefficient, e.g. 3.14 x^1 y^2
+  s <- stringi::stri_c(formatted_coef, s, sep = times)
+
+  # clean 1 and -1 from front
+  if (abs(term[["coef"]] - 1) < sqrt(.Machine$double.eps) ) s <- stringi::stri_sub(s, stringi::stri_length(times) + 2L)
+  # if (abs(term[["coef"]] + 1) < sqrt(.Machine$double.eps) ) s <- stringi::stri_c("-", stringi::stri_sub(s, 4L))
+  
+  # return
+  s
+  
+}
 
